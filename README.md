49e59654c5354ebe781ba0d2769af54de2e56be4
Andre Figueiredo <afigueiredo@greencents.com>
2016-02-16 03:21:56 -0200

    Commit of first functional version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # better-trello
-User scripts to improve Trello user experience
+Userscript to improve Trello user experience.
+
+## Features
+- **Support for Jira ticket format on cards list**: PROJ-1234 Whatever
+- **Colorize cards with label color**: as long as you use no more than one label per card
+- **Show more card lists**: defaults to 7 columns
+- **Beautify board**
+- **Fast**: yes, userscripts can (and should) also be fast
+
+## TODO
+- Configurations
+  - turn on/off features
+- Add more features
+- Remove dependency from Trello's jQuery
--- src/better-trello.js
+++ src/better-trello.js
@@ -8,32 +8,46 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    /* jshint -W097 */
-    'use strict';
+(function () {
+  'use strict';
 
-    $(document).ready(function(){
+  jQuery('head').append('<style>\
+.list-wrapper{margin:0 .14%;width:14%;min-width:110px;max-width:270px}.list-wrapper:first-child{margin-left:.1%}.list-cards{margin:0;padding:0 2%}.list-card{border-bottom:0;box-shadow:-2px 2px 5px 0 #888;margin-bottom:8px}.list-card-labels{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;opacity:.5;z-index:-1}.list-card-labels .card-label{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;font-size:12px;height:auto;line-height:normal;text-align:right;width:auto}.ticket-title{opacity:.8}@media(max-width:1200px){.ticket-title{display:none}}.list-card-details{color:#3d3d3d;min-height:45px}\
+</style>');
 
-        var css = '';
-        css += '.list-wrapper:first-child { margin-left: 0.1% }';
-        css += '.list-wrapper { margin: 0 0.14%; width: 14%; }';
-        css += '.list-cards { margin: 0; padding: 0 2%; }';
-        css += '.ticket-title { color: #555; }';
+  var betterTrello = (function() {
 
-        var cards = {
+    function getText(element) {
+      var i = element.childNodes.length;
+      while (--i) {
+        if (element.childNodes[i].nodeType === Node.TEXT_NODE) {
+          return element.childNodes[i].nodeValue;
+        }
+      }
+      return '';
+    }
 
-        };
+    function formatJiraTicket(_, element) {
+      var regexp = betterTrello.options.jiraTicketRegex;
+      var text = getText(element);
+      var ticketID = (text.match(regexp) || [text])[0];
+      if (ticketID !== text) {
+        jQuery(element)
+          .text(ticketID)
+          .append('<small class="ticket-title"> ' + text.replace(regexp, '') + '</small>');
+      }
+    }
 
+    return {
+      options: {
+        jiraTicketRegex: /^[A-z]+-\d+/
+      },
+      formatJiraCards: function () {
+        jQuery('.list-card-title').each(formatJiraTicket);
+      }
+    };
+  })();
 
-        setTimeout(function() {
-            $('.list-wrapper .list-card-title').each(function(index, element){
-                var text = element.innerText;
-                var regex = new RegExp(/^[A-z]+-\d+\s-\s/);
-                if (regex.test(text)) {
-                    $(element).text(text.match(/^[A-z]+-\d+/)[0]).append('<small class="ticket-title"> - ' + text.split(regex)[1] + '</small>');
-                }
-            });
-        }, 300);
-    });
-
+  setTimeout(betterTrello.formatJiraCards, 500);
+  setInterval(betterTrello.formatJiraCards, 1000);
 })();
