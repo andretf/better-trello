ae9780eecdfcf537f6e9767d46be81404513794c
Andre Figueiredo <afigueiredo@opsolve.com>
2016-10-16 13:17:46 -0200

    improve layout

--- src/better-trello.js
+++ src/better-trello.js
@@ -12,7 +12,7 @@
   'use strict';
 
   jQuery('head').append('<style>\
-.list-wrapper{margin:0 .14%;width:14%;min-width:110px;max-width:270px}.list-wrapper:first-child{margin-left:.1%}.list-cards{margin:0;padding:0 2%}.list-card{border-bottom:0;box-shadow:-2px 2px 5px 0 #888;margin-bottom:8px}.list-card-labels{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;opacity:.5;z-index:-1}.list-card-labels .card-label{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;font-size:12px;height:auto;line-height:normal;text-align:right;width:auto}.ticket-title{opacity:.8}@media(max-width:1200px){.ticket-title{display:none}}.list-card-details{color:#3d3d3d;min-height:45px}\
+.list-wrapper{margin:0 .14%;max-width:270px;min-width:110px;width:14%}.list-wrapper:first-child{margin-left:.1%}.list{box-shadow:-2px 2px 7px 0 rgba(0,0,0,0.6)}.list-cards{background-color:#fefefe;border-bottom:1px solid #ccc;border-top:1px solid #ccc;margin:0;padding:5px 2% 0 2%}.list-card{border:1px solid rgba(0,0,0,0.3);border-radius:4px;box-shadow:-2px 2px 6px 0 rgba(0,0,0,0.4);margin-bottom:8px}.list-card-title{padding-top:5px}.list-card-labels{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;opacity:.5;z-index:-1}.list-card-labels .card-label{bottom:0;left:0;margin:0;position:absolute;right:0;top:0;font-size:12px;height:auto;line-height:normal;text-align:right;width:auto}.ticket-title{display:block;opacity:.8}@media(max-width:1999px){.ticket-title{display:none}}.list-card-details{color:#3d3d3d;min-height:45px}\
 </style>');
 
   var betterTrello = (function() {
